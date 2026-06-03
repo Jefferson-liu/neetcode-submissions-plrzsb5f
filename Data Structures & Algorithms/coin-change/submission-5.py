922f4bc8075eaa5fc911029009818e99aaeb1b67
Jefferson Liu <35241738+Jefferson-liu@users.noreply.github.com>
2026-06-03 17:36:25 -0400

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [math.inf] * (amount + 1)
+        # fewest number of coins to make up each amount, we want to see fewest number of coins for each value up to amount and then see if its possible
+        if amount == 0:
+            return 0
+        dp[0] = 0
+        for coin in coins:
+            if coin <= amount:
+                dp[coin] = 1
+        
+        for i in range(amount + 1):
+            for coin in coins:
+                if i - coin > 0:
+                    if dp[i-coin] < math.inf:
+                        dp[i] = min(dp[i], dp[i-coin] + 1)
+        if dp[amount] < math.inf:       
+            return dp[amount]
+        return -1
+
+            
