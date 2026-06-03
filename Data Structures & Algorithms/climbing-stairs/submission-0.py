018ca65170d13806d478218cb198217dd8faf3dd
Jefferson Liu <35241738+Jefferson-liu@users.noreply.github.com>
2026-06-03 12:47:36 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 2) 
+        dp[0] = 1
+        for i in range(n):
+            #either jump one or two
+            dp[i + 1] += dp[i]
+            dp[i + 2] += dp[i]
+        return dp[n]
