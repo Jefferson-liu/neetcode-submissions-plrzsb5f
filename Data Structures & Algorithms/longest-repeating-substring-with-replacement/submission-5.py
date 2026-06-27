22b396106473d61db43ff5307ccb8ad09b185253
Jefferson Liu <35241738+Jefferson-liu@users.noreply.github.com>
2026-06-27 17:44:48 -0400

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # keep track of max character counts
+        if len(s) <= 1:
+            return 1
+        counts = [0 for _ in range(26)]
+        l = 0
+        r = 0
+        maxLen = 0
+        while r < len(s):
+            counts[ord(s[r]) - ord("A")] += 1
+            while r - l - max(counts) + 1 > k:
+                counts[ord(s[l]) - ord("A")] -= 1
+                l += 1
+            
+            maxLen = max(maxLen, r - l + 1)
+            
+            r += 1
+            
+        return maxLen
+
+                
+            
