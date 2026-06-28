43141b3f6884b170f4492928e5ba473d2381e766
Jefferson Liu <35241738+Jefferson-liu@users.noreply.github.com>
2026-06-27 20:35:47 -0400

    Add: minimum-window-with-characters - submission-11

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-11.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+class Solution:
+    
+    def minWindow(self, s: str, t: str) -> str:
+        counts = dict()
+        needed = 0
+        for char in t:
+            if char in counts:
+                counts[char] += 1
+            else:
+                counts[char] = 1
+            needed += 1
+        minLen = len(s) + 1
+        minString = ""
+        l = 0
+        r = 0
+        tempCounts = defaultdict(int)
+        while r < len(s) and l <= r:
+            if s[r] in counts:
+                if tempCounts[s[r]] < counts[s[r]]:
+                    needed -= 1
+                tempCounts[s[r]] += 1
+            while needed == 0:
+                if minLen > r - l + 1:
+                    minString = s[l:r + 1]
+                    minLen = r - l + 1
+                if s[l] in counts:
+                    tempCounts[s[l]] -= 1
+                    if counts[s[l]] > tempCounts[s[l]]:
+                        needed += 1
+                l += 1
+            r += 1
+        return minString
