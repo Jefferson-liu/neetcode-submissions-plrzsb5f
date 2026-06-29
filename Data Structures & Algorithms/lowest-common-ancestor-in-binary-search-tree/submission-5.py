61a1cc5b9a4be533c06aae0c755abbc7051c59f9
Jefferson Liu <35241738+Jefferson-liu@users.noreply.github.com>
2026-06-29 00:06:34 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-5

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-5.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        
+        def isParent(root, child):
+            if root is None:
+                return False
+            if child.val == root.val:
+                return True
+            else:
+                if child.val < root.val:
+                    return isParent(root.left, child)
+                else:
+                    return isParent(root.right, child)
+        
+        queue = deque([root])
+        parent = root
+        while queue:
+            cur = queue.popleft()
+            if isParent(cur, p) and isParent(cur, q):
+                parent = cur
+            if cur.left and q.val <= cur.val and p.val <= cur.val:
+                queue.append(cur.left)
+            if cur.right and q.val >= cur.val and p.val >= cur.val:
+                queue.append(cur.right)
+        return parent
