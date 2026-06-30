8a943cd4c157681fb6d835b6f6ed97fdb2434101
Jefferson Liu <35241738+Jefferson-liu@users.noreply.github.com>
2026-06-30 16:38:07 -0400

    Add: meeting-schedule-ii - submission-9

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-9.py
@@ -0,0 +1,36 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        # if a meeting starts, it would need a room, once it ends, the room is free
+
+        # if a meeting start time is less than a meeting end time, we want to add one
+        # if a meeting end time is more than a meeting start time, we subtract one
+        # treat it like everyone needs a room.
+        # want to know how many end times are more than start times
+
+        starts = sorted([x.start for x in intervals])
+        ends = sorted([x.end for x in intervals])
+
+        startInd = 0
+        endInd = 0
+        res = 0
+        count = 0
+
+        while startInd < len(intervals):
+            if starts[startInd] < ends[endInd]:
+                count += 1
+                startInd += 1
+            else:
+                count -= 1
+                endInd += 1
+            res = max(count, res)
+        return res
+        
+
