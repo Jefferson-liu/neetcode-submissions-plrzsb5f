fdd1d57a5d1a7a7ae1db2c85170c44acd4b1ba0d
Jefferson Liu <35241738+Jefferson-liu@users.noreply.github.com>
2026-08-27 13:17:42 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set <Integer> numSet = new HashSet<>();
+        for (int num: nums){
+            numSet.add(num);
+        }
+        return (numSet.size() != nums.length);
+        
+    }
+}
